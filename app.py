d81c36830fae9d902a1c2c82e08230f42f145fed
valliammai-tech <vmuthaiyah43@gmail.com>
2022-06-14 17:58:42 +0530

    Simple Flask App

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/",methods=['GET'])
+def index():
+    return "Hello !! Starting Machine Learning Project"
+
+if(__name__)=="__main__":
+    app.run(debug=True)
